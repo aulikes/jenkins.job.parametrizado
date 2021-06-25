f11caf4c14c2b7aa453f9eff91266bddd0864c9e
aulikes <30501765+aulikes@users.noreply.github.com>
2021-06-24 20:27:23 -0500

    Create parametrizadoDSL.groovy

---
+++ parametrizadoDSL.groovy
@@ -0,0 +1,23 @@
+job('ejemplo-job-DSL_GitHub') {
+  description ('Job creado desde el Seed Job por medio de DSL')
+  scm {
+    git('https://github.com/aulikes/jenkins.job.parametrizado.git', 'main') { node -> 
+      node / gitConfigName('aulikes')
+      node / gitConfigEmail('aulikes@hotmail.com')
+    }
+  }
+  parameters {
+    stringParam('nombre', defaultValue = 'Mike', description = 'Parámetro de cadena')
+    choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra (default)'])
+    booleanParam('agente', false)
+  }
+  triggers {
+    cron('H/7 * * * *')
+  }
+  steps {
+    shell("bash jobscript.sh")
+  }
+  publishers {
+    mailer('aulikes@hotmail.com', true, true)
+  }
+}
